d01d199b1af261873c1fcd67a5d8f73d17469186
ProsperChihimba <prosperchihimba@gmail.com>
2022-11-27 02:48:13 +0400

    Add: home screen header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:coinprofile/utils/app_layout.dart';
+import 'package:coinprofile/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +12,48 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(17)),
+            child: Column(
+              children: [
+                // App header
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: AppLayout.getHeight(17)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                width: 1.4, color: Styles.blackColor),
+                            borderRadius:
+                                BorderRadius.circular(AppLayout.getWidth(20))),
+                        child: ClipRRect(
+                          borderRadius:
+                              BorderRadius.circular(AppLayout.getWidth(20)),
+                          child: Image(
+                            image: const AssetImage("assets/proc.jpeg"),
+                            height: AppLayout.getHeight(35),
+                            width: AppLayout.getWidth(35),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Welcome message
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
