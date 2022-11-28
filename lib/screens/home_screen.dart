66dcc105648a6c0303f5fde9fa2f556d00502760
ProsperChihimba <prosperchihimba@gmail.com>
2022-11-28 10:07:32 +0400

    Add: transactions data section

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -312,6 +312,54 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
+
+          // User transactions
+          Gap(AppLayout.getHeight(15)),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: AppLayout.getWidth(17),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  width: AppLayout.getWidth(35),
+                  height: AppLayout.getHeight(35),
+                  decoration: BoxDecoration(
+                      color: const Color(0xFFdaede5),
+                      borderRadius:
+                          BorderRadius.circular(AppLayout.getHeight(16))),
+                  child: Transform.rotate(
+                    angle: 0.8,
+                    child: Icon(
+                      Icons.arrow_downward_rounded,
+                      color: Colors.green.shade700,
+                    ),
+                  ),
+                ),
+                Gap(AppLayout.getWidth(20)),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "@balanceV1",
+                      style: Styles.headerTextTwo,
+                    ),
+                    Gap(AppLayout.getHeight(5)),
+                    Text(
+                      "Received . May 13",
+                      style: Styles.normalText
+                          .copyWith(color: Colors.grey.shade400),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Text(
+                  "+0",
+                  style: Styles.headerTextTwo,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
