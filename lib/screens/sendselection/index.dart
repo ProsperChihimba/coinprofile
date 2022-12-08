538f48caddc0ea7bf59b2beb05367c4e979bfc9d
ProsperChihimba <prosperchihimba@gmail.com>
2022-12-08 21:41:52 +0400

    Add: send money selection page

---
+++ lib/screens/sendselection/index.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SendMoneySelection extends StatefulWidget {
+  const SendMoneySelection({super.key});
+
+  @override
+  State<SendMoneySelection> createState() => _SendMoneySelectionState();
+}
+
+class _SendMoneySelectionState extends State<SendMoneySelection> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("hello"),
+    );
+  }
+}
